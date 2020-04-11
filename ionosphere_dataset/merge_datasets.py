72646b4dc4b3925ff3852fd39d8ad3c008e20e56
Maria <mary.vtyurina@gmail.com>
2020-04-11 15:02:01 +0300

    added TZ conversion for SY951 sonde

--- ionosphere_dataset/merge_datasets.py
+++ ionosphere_dataset/merge_datasets.py
@@ -11,7 +11,7 @@ sondes = {
           'TO536' : ((35.71, 139.45),[x for x in range(2001, 2021)]),
           'YG431' : ((31.20, 130.62),[x for x in range(1992, 2021)]),
           'OK426' : ((26.28, 127.81),[x for x in range(1992, 2021)])
-#           'SY951' : ((-69.00, 39.59),[x for x in range(1996, 2006)])
+          'SY951' : ((-69.00, 39.59),[x for x in range(1996, 2006)])
          }
 
 numdays = 15
@@ -78,7 +78,9 @@ def walk_earthquakes(df_s, earthquakes_folder):
     df_s_columns = df_s.columns.values.tolist()
     df_res = pd.DataFrame(columns=df_s_columns)
     
-    earthquakes_list = os.listdir(earthquakes_folder)       
+    earthquakes_list = os.listdir(earthquakes_folder)
+    print(earthquakes_list)
+    e_count = 0
     for name_e in earthquakes_list:
         path = earthquakes_folder +'/'+ name_e
         print(path)
@@ -89,13 +91,17 @@ def walk_earthquakes(df_s, earthquakes_folder):
         # extract date
         df_e['date'] = [datetime.datetime.date(d) for d in df_e['time']] 
         df_s['date'] = [datetime.datetime.date(d) for d in df_s['time']] 
-
+        
         for d in df_e['date']:
+            if d not in df_s.date.unique(): #checking if sonde was working on the day of the earthquake
+                continue
+            e_count += 1
             date_list = [d - datetime.timedelta(days=x) for x in range(numdays)]
             df_dates = pd.DataFrame(date_list, columns =['date'])
             df_temp = df_s[df_s.date.isin(df_dates.date)]
             df_res = pd.concat([df_res, df_temp]).drop_duplicates()
         print('df_res size', len(df_res))
+    print('{count} earthquakes matched with current sonde'.format(count = e_count))
 
     df_res = df_res.sort_values(by=['time'])
     return df_res
@@ -113,15 +119,21 @@ def walk_sondes(sondes_folder, earthquakes_folder):
         Saves the merged data to a new folder. Each file corresponds to a single sonde.
     """
     sondes_names = os.listdir(sondes_folder)
+#     for i in os.walk('sondes_japan_data'):
+#         folder_sondes.append(i)
     print(sondes_names)
     for name_s in sondes_names:
+        if '.csv' not in name_s:
+            continue
         path = sondes_folder +'/'+ name_s
+        print(path)
         df_s = pd.read_csv(path, sep=',')
-#         # debug
-#         df_s = df_s.head(50)
-
-        df_s = clean_time(df_s)
-        df_s = df_s.apply(convert_tz, axis=1)
+        df_s = clean_time(df_s)            
+        if "SY951" in name_s:
+            df_s = df_s.apply(convert_tz, tz_old = 'Antarctica/Syowa',  axis=1)
+        else:
+            df_s = df_s.apply(convert_tz, axis=1)
+            
         df_res = walk_earthquakes(df_s, earthquakes_folder)
 
         sonde_name = name_s.split('.')[0]
