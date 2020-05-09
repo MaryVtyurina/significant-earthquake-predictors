7d6710ef5736a34ac89faa801eb41085fbcf2eb8
Maria <mary.vtyurina@gmail.com>
2020-05-09 15:29:07 +0300

    Models and feature selection

--- earthquake_dataset/create_earthquake_dataset.py
+++ earthquake_dataset/create_earthquake_dataset.py
@@ -1,26 +1,9 @@
 import csv
 from urllib.request import urlopen
-import pandas as pd
-	
- def get_earthquake_data_csv(year, mag,  filepath):
- 	"""
-        Downloads earthquake data from earthquake.usgs.gov for 1 year with 
-
-        Arguments:
-        year - year, data will be collected from 01.01 00:00:00 to 12.12 23:59:59 of this year
-        mag - lower border of the magnitude of the earthquakes
-        filepath - the path to the directory in which the dataset will be saved
-
-        Returns:
-        saves the csv file to the dataset and prints an info message
-    """
-    url = 'https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={year}-01-01%2000%3A00%3A00&endtime={year}-12-24%2023%3A59%3A59&minmagnitude={mag}&orderby=time'.format(year = year, mag = mag)
-    df = pd.read_csv(url)
-    df.to_csv(filepath + 'earthquake_{year}.csv'.format(year = year), sep='\t')
-    print('Data for the {year} was saved to earthquake_{year}.csv'.format(year = year))
+import pandas as pd   
     
 def get_earthquake_dataset(start_year, end_year, path):
-	"""
+    """
         Downloads earthquake data from earthquake.usgs.gov for a sertain period of time
 
         Arguments:
@@ -28,7 +11,13 @@ def get_earthquake_dataset(start_year, end_year, path):
         mag - lower border of the magnitude of the earthquakes
         path - the path to the directory in which the dataset will be saved
     """
+    df_res = pd.DataFrame()
     for y in range(start_year, end_year+1):
-        get_earthquake_data_csv(y, 4.5, path)
+        print(y)
+        url = 'https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={year}-01-01%2000%3A00%3A00&endtime={year}-12-24%2023%3A59%3A59&minmagnitude={mag}&orderby=time'.format(year = year, mag = mag)
+        df_eq = pd.read_csv(url)
+        df_eq = get_earthquake_data_csv(y, 5.0)
+        df_res = pd.concat([df_res, df_e])
+    df_res.to_csv(path, index=False)
         
-get_earthquake_dataset(1992, 2020, 'earthquakes_data/')
+get_earthquake_dataset(1992, 2020, 'earthquakes_data.csv')
---
+++ ionosphere_dataset/NOAA/README.md
@@ -0,0 +1,3 @@
+You can download missing datasets from https://drive.google.com/open?id=1Ef6IYsBlYz5JsRC6xpXPP1yx9smv2LdM
+
+Download and put to ionoshere_dataset folder, then unzip the archive.
